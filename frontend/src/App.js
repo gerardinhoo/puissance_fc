a94886fa63ab723239f51e7aa136b4664303d17f
Gerard Eklu <gerard_eklu@homedepot.com>
2020-11-23 23:56:21 -0500

    Implemented the react bootstrap, bootswatch, the header and footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="App">
-     something 
-    </div>
+    <>
+      <Header />
+      <main>
+        <Container>
+        <h2 className='py-3'>Welcome To Puissance FC</h2>
+        </Container>
+     </main>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {Container, Col, Row} from 'react-bootstrap';
+
+const Footer = () => {
+  return (
+    <>
+     <Container>
+       <Row>
+         <Col className='text-center py-3'>Copyright &copy; Puissance FC</Col>
+       </Row>
+     </Container>
+    </>
+  )
+}
+
+export default Footer;
