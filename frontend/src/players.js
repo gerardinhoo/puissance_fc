4f33e8e545f49c961d84d1b1d3d86bd9219f1977
Gerard Eklu <gerard_eklu@homedepot.com>
2020-11-24 00:49:37 -0500

    Implemented the temporary player profiles data - changed padding for the footer

---
+++ frontend/src/players.js
@@ -0,0 +1,39 @@
+const players = [
+  {
+    id: 1,
+    firstName: 'Leo',
+    lastName: 'Zamudio',
+    image: '../public/images/Leo.jpeg',
+    Nationality: 'Mexico'
+  },
+  {
+    id: 2,
+    firstName: 'Cory',
+    lastName: 'Coryyy',
+    image: '../public/images/Cory.jpeg',
+    Nationality: 'Usa'
+  },
+  {
+    id: 3,
+    firstName: 'Eric',
+    lastName: 'Estrada',
+    image: '../public/images/Eric.jpeg',
+    Nationality: 'Mexico'
+  },
+  {
+    id: 3,
+    firstName: 'Jordan',
+    lastName: 'Coperland',
+    image: '../public/images/Jordan.jpeg',
+    Nationality: 'United State'
+  },
+  {
+    id: 4,
+    firstName: 'Khalil',
+    lastName: 'Khalilll',
+    image: '../public/images/Khalil.jpeg',
+    Nationality: 'Mali'
+  }
+]
+
+export default players;
